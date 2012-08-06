1f7a4a34de91581cbf5eff059c38590290e9e385
sakuramilk <c.sakuramilk@gmail.com>
2012-08-07 01:45:23 +0900

    boot movie support

--- sbin/bootanimation.sh
+++ sbin/bootanimation.sh
@@ -2,7 +2,7 @@
 
 if [ -f /data/local/bootanimation.bin ]; then
   /data/local/bootanimation.bin
-elif [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ]; then
+elif [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ] || [ -f /data/local/bootsound.mp4 ]; then
   /sbin/bootanimation
 else
   /system/bin/samsungani
